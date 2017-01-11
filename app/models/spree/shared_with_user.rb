6f1f2b50d8e3606316bcfe255ade443598e04a75
Akshat Singhal <akshat@vinsol.com>
2017-01-11 13:13:40 +0530

    Display sharedlists shared with a user on account page

--- app/models/spree/shared_with_user.rb
+++ app/models/spree/shared_with_user.rb
@@ -1,9 +1,8 @@
 class Spree::SharedWithUser < ActiveRecord::Base
   belongs_to :user, class_name: Spree.user_class
   belongs_to :sharedlist
-  # belongs_to :shared_by, class_name: Spree.user_class
 
-  validates :uniqueness, :user_id, scope: :sharedlist_id, message: Spree.t(:sharedlist_shared_with_user)
+  validates :user_id, uniqueness: { scope: :sharedlist_id, message: Spree.t(:sharedlist_shared_with_user) }
 
   after_create :send_mail_to_recipient
 
--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,3 +1,5 @@
 Spree.user_class.class_eval do
   has_many :sharedlists, class_name: Spree::Sharedlist
+  has_many :shared_with_user
+  has_many :shared_sharedlists, through: :shared_with_user, source: :sharedlist
 end
